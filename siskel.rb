7375be35cea83bb36182bc2b487676927f2855f0
Diego Lugo <diego.lugo.06@gmail.com>
2015-10-21 23:23:28 -0400

    fix broken via tests

--- siskel.rb
+++ siskel.rb
@@ -1,10 +1,37 @@
 require 'httparty'
 
 class Siskel
-  attr_reader :title
+  attr_reader :rating, :year, :plot, :title
 
-  def initialize(title)
-    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
+  def initialize(title, options = {})
+    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{options[:year]}&plot=#{options[:plot]}")
     @title = @movie['Title']
+    @rating = @movie['Rated']
+    @year = @movie['Year']
+    @plot = @movie['Plot']
   end
+
+  #refactor to put in the reader and initialize method
+  def title
+    if @title == nil
+      "Movie not found!"
+    else
+      @title
+    end
+  end
+
+  def consensus
+    if @movie['Metascore'].to_i.between?(51,100)
+      @consensus = "Thumbs Up"
+    elsif @movie['Metascore'].to_i.between?(0,50)
+      @consensus = "Thumbs Down"
+    else
+      "Metascore has to be between 0 - 100."
+    end
+  end
+
+
+  # def movie_existence
+  #    puts "Movie not found!" if @title == nil
+  # end
 end
